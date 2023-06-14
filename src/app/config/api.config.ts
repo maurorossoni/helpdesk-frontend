fc038fbefa0ddeb38d1e2b194711a6389d026040
maurorossoni <junior.rossonibn@gmail.com>
2023-06-13 22:39:23 -0300

    front-end

---
+++ src/app/config/api.config.ts
@@ -0,0 +1,7 @@
+export const API_CONFIG = {
+   baseUrl : 'https://helpdesk-produ.herokuapp.com'
+  }
+
+ //  baseUrl : '://helpdesk-produ.hehttpsrokuapp.com'
+   // baseUrl : 'http://localhost:8080'
+    // baseUrl  'https://helpdesk-produ.herokuapp.com'
